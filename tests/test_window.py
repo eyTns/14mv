c9f6202c92e9a5a88772e700b562fa8b52e9eff0
eyTns <jihan4011@naver.com>
2023-09-02 00:52:53 +0900

    이미지 비교 테스트 추가 (실패함)

--- tests/test_window.py
+++ tests/test_window.py
@@ -1,6 +1,9 @@
 import pytest
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from window.window import MyWindow
+from window.utils import compare_image_same
+import os
+import sys
 
 @pytest.fixture
 def app():
@@ -8,9 +11,22 @@ def app():
     yield app
     app.quit()
 
+
 def test_window_appears(app):
     window = MyWindow()
     # qtbot.addWidget(window)  # Required for interacting with the window
     window.show()
     assert isinstance(window, QMainWindow)
+
+
+def test_image_same():
+    # images_root path: C:\Users\james\Documents\GitHub\Minesweeper-Variants\images
+    root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    image_path_1 = os.path.join(root_path, "images", "cell_blank.png")
+    image_path_2 = os.path.join(root_path, "images", "cell_blank_2.png")
+    image_path_3 = os.path.join(root_path, "screenshot_region.png")
+    assert compare_image_same(image_path_1, image_path_2)
+    assert compare_image_same(image_path_1, image_path_3)
+    assert compare_image_same(image_path_2, image_path_2)
+
 
--- window/window.py
+++ window/window.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QGridLayout, QFrame
+from window.utils import capture_window_screenshot, find_all_templates_in_screenshot
 
 import random
 
@@ -39,8 +40,46 @@ class MyWindow(QMainWindow):
                 grid_layout.addWidget(label, row, col)
 
         # Third Frame
-        frame3 = QLabel("Bye")
+        window_title = "Minesweeper Variants"
+        save_path = f"{window_title}.png"
+        capture_window_screenshot(window_title, save_path)
+        if capture_window_screenshot(window_title, save_path):
+            print(f"Screenshot saved as {save_path}")
+        else:
+            print("Failed to capture screenshot")
+        frame3 = QLabel()
+        frame3.setPixmap(QPixmap(save_path))
         main_layout.addWidget(frame3)
+
+        # Fourth Frame
+        screenshot_path = save_path
+        template_path = "images/cell_blank.png"
+        positions = find_all_templates_in_screenshot(screenshot_path, template_path)
+
+        if positions:
+            print(f"Found {len(positions)} occurrences of the template image at positions:")
+            for position in positions:
+                print(position)
+        else:
+            print(f"Template {template_path} not found in the screenshot.")
+        
+        # Fifth Frame
+        for i in range(1, 9+1):
+            screenshot_path = save_path
+            template_path = f"images/cell_{i}.png"
+            positions = find_all_templates_in_screenshot(screenshot_path, template_path)
+
+            if positions:
+                print(f"Found {len(positions)} occurrences of the template cell {i} at positions:")
+                for position in positions:
+                    print(position)
+            else:
+                print(f"Template {template_path} not found in the screenshot.")
+
+
+        # Sixth Frame
+        frame_last = QLabel("Bye")
+        main_layout.addWidget(frame_last)
 
         # Fix the window size
         self.setFixedSize(self.sizeHint())
