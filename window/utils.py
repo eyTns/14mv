7bc987cfb0eca2fe5216216438afce27ca74821a
eyTns <jihan4011@naver.com>
2023-09-05 21:25:46 +0900

    스크린샷의 36칸 각각에 대해 이미지 인식

--- window/utils.py
+++ window/utils.py
@@ -1,6 +1,8 @@
 import pygetwindow as gw
 from PIL import Image, ImageGrab
 import cv2
+import os
+import tempfile
 
 
 def capture_window_screenshot(window_title, save_path):
@@ -53,12 +55,41 @@ def find_template_in_screenshot(screenshot_path, template_path):
         return None
 
 
-size_to_position_dict = {
+# Define the size-to-initial-position dictionary
+size_to_initial_position_dict = {
     5: (395, 234),
     6: (370, 209),
     7: (345, 184),
     8: (320, 159)
 }
+
+
+def get_cropped_cell_coordinates(size):
+    # Check if the provided size is valid
+    if size not in size_to_initial_position_dict:
+        raise ValueError("Invalid size. Size should be 5, 6, 7, or 8.")
+
+    # Get the initial position based on the provided size
+    initial_x, initial_y = size_to_initial_position_dict[size]
+
+    # Define the fixed increments
+    x_increment, y_increment = 50, 50
+
+    # Initialize a list to store the coordinates of cropped cells
+    cell_coordinates = []
+
+    # Calculate the coordinates for all cropped cells
+    for row in range(size):
+        row_coordinates = []  # Inner list for each row
+        for col in range(size):
+            x1 = initial_x + col * x_increment
+            y1 = initial_y + row * y_increment
+            x2 = x1 + x_increment
+            y2 = y1 + y_increment
+            row_coordinates.append((x1, y1, x2, y2))
+        cell_coordinates.append(row_coordinates)  # Append row to the list
+
+    return cell_coordinates
 
 
 def find_all_templates_in_screenshot(screenshot_path, template_path):
@@ -81,7 +112,7 @@ def find_all_templates_in_screenshot(screenshot_path, template_path):
                 # Crop the screenshot to the same size as the template
                 screenshot_region = screenshot.crop((x, y, x + template_width, y + template_height))
 
-                if (x, y) in size_to_position_dict.values():
+                if (x, y) in size_to_initial_position_dict.values():
                     screenshot_region.save(f"screenshot_region_{x}_{y}.png")
                     
                 # Compare the cropped region with the template
@@ -111,7 +142,77 @@ def MSE_of_images(image_path_1, image_path_2):
     # Calculate the Mean Squared Error (MSE)
     mse = ((image1 - image2) ** 2).mean()
 
-    cv2.compare_ssim(image1, image2, multichannel=True)
-
     print(f'Mean Squared Error: {mse}')
     return mse
+
+
+def find_best_template_filename(captured_cell_path):
+    # Load the captured cell image
+    captured_cell = cv2.imread(captured_cell_path)
+
+    # Construct the full path to the directory where template images are located
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    templates_directory = os.path.join(current_directory, '..', 'images')
+
+    # Initialize variables to keep track of the best template and its MSE
+    best_template_filename = None
+    min_mse = float('inf')
+
+    # Iterate through all template images in the directory
+    for template_filename in os.listdir(templates_directory):
+        # Load the template image
+        template = cv2.imread(os.path.join(templates_directory, template_filename))
+
+        # Calculate the Mean Squared Error (MSE) between the captured cell and the template
+        mse = ((captured_cell - template) ** 2).mean()
+
+        # Check if this template has a lower MSE than the current minimum
+        if mse < min_mse:
+            min_mse = mse
+            best_template_filename = template_filename
+
+    # If a best template was found, return its filename and the MSE
+    if best_template_filename is not None:
+        cv2.imwrite('best_template.png', cv2.imread(os.path.join(templates_directory, best_template_filename)))
+        return best_template_filename, min_mse
+    else:
+        return None, None
+
+
+def find_best_fit_cells(screenshot_path, cell_size):
+    # Get the cropped cell coordinates for the specified size
+    cell_coordinates = get_cropped_cell_coordinates(cell_size)
+
+    # Load the screenshot image
+    screenshot = cv2.imread(screenshot_path)
+
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    templates_directory = os.path.join(current_directory, '..', 'images')
+
+    # Initialize a list to store the best-fit cell filenames
+    best_fit_filenames = []
+
+    # Iterate through the cropped cell coordinates and find the best-fit templates
+    for row in cell_coordinates:
+        row_best_fit = []  # List to store best-fit template filenames for the row
+        for coordinates in row:
+            # Crop the cell from the screenshot
+            x1, y1, x2, y2 = coordinates
+            captured_cell = screenshot[y1:y2, x1:x2]
+
+            # Save the captured cell as an image with a unique name
+            temp_dir = tempfile.gettempdir()
+            captured_cell_filename = os.path.join(temp_dir, f'captured_cell_{coordinates[0]}_{coordinates[1]}.png')
+            cv2.imwrite(captured_cell_filename, captured_cell)
+
+            # Find the best template for the captured cell
+            best_template_filename, min_mse = find_best_template_filename(captured_cell_filename)
+            row_best_fit.append(best_template_filename)
+
+        best_fit_filenames.append(row_best_fit)
+
+    return best_fit_filenames
+
+
+
+
--- window/window.py
+++ window/window.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QGridLayout, QFrame
-from window.utils import capture_window_screenshot, find_all_templates_in_screenshot
+from window.utils import capture_window_screenshot, find_all_templates_in_screenshot, find_best_fit_cells
 
 import random
 
@@ -30,14 +30,15 @@ class MyWindow(QMainWindow):
         grid_layout.setContentsMargins(10, 10, 10, 10)
         frame2.setLayout(grid_layout)
 
-        for row in range(8):
-            for col in range(8):
-                # image_path = f"images/cell_{(row*8 + col)%9+1}.png"
-                image_path = f"images/cell_{random.randint(1, 9)}.png"
-                pixmap = QPixmap(image_path)
-                label = QLabel()
-                label.setPixmap(pixmap)
-                grid_layout.addWidget(label, row, col)
+        # 랜덤한 숫자를 표시
+        # for row in range(8):
+        #     for col in range(8):
+        #         # image_path = f"images/cell_{(row*8 + col)%9+1}.png"
+        #         image_path = f"images/cell_{random.randint(1, 9)}.png"
+        #         pixmap = QPixmap(image_path)
+        #         label = QLabel()
+        #         label.setPixmap(pixmap)
+        #         grid_layout.addWidget(label, row, col)
 
         # Third Frame
         window_title = "Minesweeper Variants"
@@ -47,22 +48,44 @@ class MyWindow(QMainWindow):
             print(f"Screenshot saved as {save_path}")
         else:
             print("Failed to capture screenshot")
+
         frame3 = QLabel()
         frame3.setPixmap(QPixmap(save_path))
         main_layout.addWidget(frame3)
 
+        # # Fourth Frame
+        # screenshot_path = save_path
+        # template_path = "images/cell_blank.png"
+        # positions = find_all_templates_in_screenshot(screenshot_path, template_path)
+
+        # if positions:
+        #     print(f"Found {len(positions)} occurrences of the template image at positions:")
+        #     for position in positions:
+        #         print(position)
+        # else:
+        #     print(f"Template {template_path} not found in the screenshot.")
+
         # Fourth Frame
         screenshot_path = save_path
-        template_path = "images/cell_blank.png"
-        positions = find_all_templates_in_screenshot(screenshot_path, template_path)
+        cell_size = 6
+        best_fit_cells = find_best_fit_cells(screenshot_path, cell_size)
+        print(best_fit_cells)
 
-        if positions:
-            print(f"Found {len(positions)} occurrences of the template image at positions:")
-            for position in positions:
-                print(position)
-        else:
-            print(f"Template {template_path} not found in the screenshot.")
-        
+        frame4 = QFrame()
+        layout = QVBoxLayout()
+        label = QLabel()
+        label.setText("\n".join("Row {}: {}".format(i, ", ".join(cell for cell in row if cell is not None)) for i, row in enumerate(best_fit_cells)))
+        layout.addWidget(label)
+        frame4.setLayout(layout)
+        main_layout.addWidget(frame4)
+
+
+
+
+
+
+
+
         # # Fifth Frame
         # for i in range(1, 9+1):
         #     screenshot_path = save_path
